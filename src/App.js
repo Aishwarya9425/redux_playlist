77c821eeaeb943c7caec7c93d404d49b549f68e7
Aishwarya <aishwarya9425@gmail.com>
2023-10-18 19:47:18 +0530

    code update - redux tool kit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./styles.css";
+import { useDispatch } from "react-redux";
+import MoviePlaylist from "./components/MoviePlaylist";
+import SongPlaylist from "./components/SongPlaylist";
+import { reset } from "./store";
 
 export default function App() {
+  const dispatch = useDispatch();
+
+  const handleResetClick = () => {
+    dispatch(reset());
+  };
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="container is-fluid">
+      <button onClick={() => handleResetClick()} className="button is-danger">
+        Reset Both Playlists
+      </button>
+      <hr />
+      <MoviePlaylist />
+      <hr />
+      <SongPlaylist />
     </div>
   );
 }
